7c1a874cb2b4e9eaf5fa2b3c58e392ca77c6a499
Dustin Bench <dbench@dcsdk12.org>
2017-11-09 11:51:10 -0700

    Dropped code into a function call named ios_conf()

--- CiscoIOS_Config.py
+++ CiscoIOS_Config.py
@@ -9,46 +9,53 @@
 
 --  Version 1.1
 --  Modified by:  Dustin Bench, 8/30/17 (Added a feature that will bypass an SSH Exception using a try/except block.)
+
+--  Version 1.0
+--  Modified by:  Dustin Benc, 11/9/17 (Dropping all code into a function for import ability)
 """
 
 import sys
 from netmiko import ConnectHandler
 from paramiko.ssh_exception import SSHException
 
-file = open('c:\python27\list1.csv', 'r')
+def ios_conf():
 
-print '\n\n>>>>>>>>>>>>>>> All Switches Python Script<<<<<<<<<<<<<<<<<<<'
-print '\n'
+    file = open('c:\python27\list1.csv', 'r')
 
-#Create configuration command variable
-config_commands = ['ntp server 10.10.10.10', 'end', 'wr mem']
+    print '\n\n>>>>>>>>>>>>>>> All Switches Python Script<<<<<<<<<<<<<<<<<<<'
+    print '\n'
 
-for line in file:
-#Create a list out of list1_Network_Switch_Report.csv
-    switch_list = line.strip().split(',')
-#Place each line of list1 into a dictionary
-    device_info = {}
-    device_info['device_type'] = switch_list[1]
-    device_info['ip'] = switch_list[2]
-    device_info['username'] = switch_list[3]
-    device_info['password'] = switch_list[4]
-    #create python list that includes all of the items in the device_info dictionary
-    all_devices = [device_info]
-# Loop through device_info and apply config_commands to each element
-    for a_device in all_devices:
-        try:
-            net_connect = ConnectHandler(**a_device)
-            output = net_connect.send_config_set(config_commands)
-            print "\n\n>>>>>>>>>>>>>>> Device {0} <<<<<<<<<<<<<<<".format(a_device['device_type'])
-            print output
-        except SSHException:
-            print "\n\n>>>>>>>>>>>>>>> Device {0} <<<<<<<<<<<<<<<".format(a_device['ip'])
-            print 'Connection to device failed.  Please check TACACS configuration, exiting.'
-            continue
-            sys.exit()
-    print ">>>>>>>>>> END <<<<<<<<<<"
+    #Create configuration command variable
+    config_commands = ['ntp server 10.10.10.10', 'end', 'wr mem']
 
-file.close()
+    for line in file:
+        #Create a list out of list1_Network_Switch_Report.csv
+        switch_list = line.strip().split(',')
+        #Place each line of list1 into a dictionary
+        device_info = {}
+        device_info['device_type'] = switch_list[1]
+        device_info['ip'] = switch_list[2]
+        device_info['username'] = switch_list[3]
+        device_info['password'] = switch_list[4]
+        #create python list that includes all of the items in the device_info dictionary
+        all_devices = [device_info]
+        # Loop through device_info and apply config_commands to each element
+        for a_device in all_devices:
+            try:
+                net_connect = ConnectHandler(**a_device)
+                output = net_connect.send_config_set(config_commands)
+                print "\n\n>>>>>>>>>>>>>>> Device {0} <<<<<<<<<<<<<<<".format(a_device['device_type'])
+                print output
+            except SSHException:
+                print "\n\n>>>>>>>>>>>>>>> Device {0} <<<<<<<<<<<<<<<".format(a_device['ip'])
+                print 'Connection to device failed.  Please check TACACS configuration, exiting.'
+                continue
+                sys.exit()
+        print ">>>>>>>>>> END <<<<<<<<<<"
 
-print "\n\n"	
-print ">>>>>>>>>>>>>>>>>>>>>>>Script has completed.  Good Bye<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+    file.close()
+
+    print "\n\n"
+    print ">>>>>>>>>>>>>>>>>>>>>>>Script has completed.  Good Bye<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+
+ios_conf()
